3f2d97a51fbe4d02f893b890ed4435f4842fa886
Davidzam07 <zamorad367@gmail.com>
2025-10-30 15:47:08 -0300

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import  ItemListContainer  from "./components/ItemListContainer";
 import  ItemDetailContainer from "./components/ItemDetailContainer";
+import { getFirestore, getDoc, doc } from "firebase/firestore";
 
 function App() {
+  const db = getFirestore();
+  const docRef = doc(db, "items", "kyzGAy95a5w86wh0fdVM");
+  
+  getDoc(docRef).then((snapshot) => {
+    console.log(snapshot);
+   // console.log(snapshot.data(), snapshot.id);
+  });
+
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-      
         <Route path="/" element={<ItemListContainer greeting="Catálogo de productos" />} />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />     
         <Route path="/item/:id" element={<ItemDetailContainer />} />
